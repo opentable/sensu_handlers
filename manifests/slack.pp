ef86fbc017db4c64700ccfa07b1f994020e01ced
lmedina-ot <lmedina@opentable.com>
2025-07-23 15:25:36 -0600

    Update slack.pp config to include bot token

--- manifests/slack.pp
+++ manifests/slack.pp
@@ -19,6 +19,7 @@ class sensu_handlers::slack (
       webhook_url       => $webhook_url,
       use_default_pager => $use_default_pager,
       compact_message   => $compact_message,
+      slack_bot_token   => $slack_bot_token,
     }
   }
 
